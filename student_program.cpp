6487789ee96eb5fecfaa16ee0773efec985e01d8
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-19 11:55:07 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+void GetMatrix( int **value, int *row, int *col ) ;
+void PrintMatrix( int **value, int row, int col ) ;
+
 int main() {
-    //YOUR CODE HERE
+    int *data, m = 3, n = 3 ;
+    data = new int[ m * n ] ;
+    GetMatrix( &data, &m, &n ) ;
+    PrintMatrix( &data, m, n ) ;
     return 0 ;
+}//end function
+
+void PrintMatrix( int **value, int row, int col ) {
+    for( int i = 0 ; i < row ; i++ ) {
+        for( int j = 0 ; j < col ; j++ )
+            printf( "%d ", (*value)[ i * col + j ] ) ;
+        printf( "\n" ) ;
+    }
+    printf( "\n" ) ;
+}//end function
+
+void GetMatrix( int **value, int *row, int *col ) {
+    scanf( "%d %d", row, col ) ;
+    *value = new int[ *row * *col ] ;
+    for( int i = 0 ; i < *row ; i++ ) {
+        for( int j = 0 ; j < *col ; j++ ) {
+            scanf( "%d", &(*value)[ i * *col + j ] );
+        }
+    }
 }//end function
